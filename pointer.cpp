98470cc9f3360bd9ae69a5de3b662f1fd1eaa630
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-05 15:32:13 +0700

    memberikan akses public nim mahasiswa

--- pointer.cpp
+++ pointer.cpp
@@ -12,3 +12,34 @@ class staff {
     public:
     int nidn;
 };
+int main(){
+    Dosen ds;
+    ds.nama = "Giga";
+    ds.tampilNama();
+
+    Dosen *pds = &ds;
+    pds->nama = "Joko";
+    cout << "Alamat memori = " << pds << endl;
+    dsref.tampilnama();
+
+    Dosen *pds = &ds;
+    pds->nama = "Reza";
+    cout << "Alamat memori = " << pds << endl;
+    pds-> tampilnama();
+
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout "alamat memori a = " << &a << endl;
+    cout "alamat memori c = " << &c << endl;
+    cout "cetak c = " << *c << endl;
+    c = &b;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c < endl;
+
+
+}
